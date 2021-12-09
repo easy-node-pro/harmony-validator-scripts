7802c876f9c41938a57a393c343867baeaf49fbc
PatrickMogul <patrick@easynode.one>
2021-12-09 16:44:08 -0600

    Stats

--- stats/README.md
+++ stats/README.md
@@ -1,15 +1,8 @@
 # stats 
-### This contains our versions of the stats.sh script in our guide.
-The commands below will download and install our stats.sh script to use later to check your stats including cpu, disk, harmony version & block status. Choose only the shard you're going to run on below 0-3:
-
-wget https://raw.githubusercontent.com/slugom-crypto/harmony-validator-scripts/main/stats/shard_0.sh && mv shard_0.sh stats.sh && chmod +x stats.sh
-
-wget https://raw.githubusercontent.com/slugom-crypto/harmony-validator-scripts/main/stats/shard_1.sh && mv shard_1.sh stats.sh && chmod +x stats.sh
-
-wget https://raw.githubusercontent.com/slugom-crypto/harmony-validator-scripts/main/stats/shard_2.sh && mv shard_2.sh stats.sh && chmod +x stats.sh
-
-wget https://raw.githubusercontent.com/slugom-crypto/harmony-validator-scripts/main/stats/shard_3.sh && mv shard_3.sh stats.sh && chmod +x stats.sh
+### This contains our versions of the stats.py script in our guide.
+Setup the stats.py file on your system. Add your shard # into the script.
+- Run the script with:
+`python3 stats.py`
 
 ### Customizations
-- Current scripts in this repo will only work in the same folder with your harmony binary file.
-- Feel free to upgrade the contents of the script for your environment so they can run from any location.
+- This script assumes you're using a ~/harmony folder, if not modify the path in the script
--- stats/stats.py
+++ stats/stats.py
@@ -3,29 +3,28 @@ import subprocess
 from subprocess import Popen, PIPE, run
 from ast import literal_eval
 
-# Set your shard # here
-shard = 3
+ourShard = 3
 
 remote_shard_0 = ['/home/serviceharmony/harmony/hmy', 'blockchain', 'latest-headers', f'--node=https://api.s0.t.hmny.io']
 result_remote_shard_0 = run(remote_shard_0, stdout=PIPE, stderr=PIPE, universal_newlines=True)
 remote_data_shard_0 = json.loads(result_remote_shard_0.stdout)
-remote_shard = ['/home/serviceharmony/harmony/hmy', 'blockchain', 'latest-headers', f'--node=https://api.s{shard}.t.hmny.io']
+remote_shard = ['/home/serviceharmony/harmony/hmy', 'blockchain', 'latest-headers', f'--node=https://api.s{ourShard}.t.hmny.io']
 result_remote_shard = run(remote_shard, stdout=PIPE, stderr=PIPE, universal_newlines=True)
 remote_data_shard = json.loads(result_remote_shard.stdout)
 local_shard = ['/home/serviceharmony/harmony/hmy', 'blockchain', 'latest-headers']
 result_local_shard = run(local_shard, stdout=PIPE, stderr=PIPE, universal_newlines=True)
 local_data_shard = json.loads(result_local_shard.stdout)
-
-print()
-print(f"Shard {shard} Sync Status:")
-print(f"Local Server  - Epoch {local_data_shard['result']['shard-chain-header']['epoch']} - Shard {local_data_shard['result']['shard-chain-header']['shardID']} - Block {literal_eval(local_data_shard['result']['shard-chain-header']['number'])}")
-print(f"Remote Server - Epoch {remote_data_shard['result']['shard-chain-header']['epoch']} - Shard {remote_data_shard['result']['shard-chain-header']['shardID']} - Block {literal_eval(remote_data_shard['result']['shard-chain-header']['number'])}")
 print()
 print(f"Shard 0 Sync Status:")
 print(f"Local Server  - Epoch {local_data_shard['result']['beacon-chain-header']['epoch']} - Shard {local_data_shard['result']['beacon-chain-header']['shardID']} - Block {literal_eval(local_data_shard['result']['beacon-chain-header']['number'])}")
 print(f"Remote Server - Epoch {remote_data_shard_0['result']['shard-chain-header']['epoch']} - Shard {remote_data_shard_0['result']['shard-chain-header']['shardID']} - Block {literal_eval(remote_data_shard_0['result']['shard-chain-header']['number'])}")
 print()
-print(f"Local Server Stats, Shard {shard}:")
+if ourShard > 0:
+    print(f"Shard {ourShard} Sync Status:")
+    print(f"Local Server  - Epoch {local_data_shard['result']['shard-chain-header']['epoch']} - Shard {local_data_shard['result']['shard-chain-header']['shardID']} - Block {literal_eval(local_data_shard['result']['shard-chain-header']['number'])}")
+    print(f"Remote Server - Epoch {remote_data_shard['result']['shard-chain-header']['epoch']} - Shard {remote_data_shard['result']['shard-chain-header']['shardID']} - Block {literal_eval(remote_data_shard['result']['shard-chain-header']['number'])}")
+    print()
+print(f"Local Server Stats, Shard {ourShard}:")
 print("Beacon Chain Header:")
 print(f"Epoch: {local_data_shard['result']['beacon-chain-header']['epoch']}")
 print(f"Block: {literal_eval(local_data_shard['result']['beacon-chain-header']['number'])}")
@@ -36,7 +35,7 @@ print(f"Epoch: {local_data_shard['result']['shard-chain-header']['epoch']}")
 print(f"Block: {literal_eval(local_data_shard['result']['shard-chain-header']['number'])}")
 print(f"Shard: {local_data_shard['result']['shard-chain-header']['shardID']}")
 print()
-print(f"Remote Blockchain Stats, Shard {shard}:")
+print(f"Remote Blockchain Stats, Shard {ourShard}:")
 print("Beacon Chain Header:")
 print(f"Epoch: {remote_data_shard['result']['beacon-chain-header']['epoch']}")
 print(f"Block: {literal_eval(remote_data_shard['result']['beacon-chain-header']['number'])}")
