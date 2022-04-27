ff45b6fcea14e8040e4677ead1712162ace06b8d
Patrick Mogul <patrick@easynode.one>
2022-04-27 14:50:34 -0500

    Update stats.py

--- stats/stats.py
+++ stats/stats.py
@@ -14,61 +14,41 @@ remote_shard_0 = [f'{harmonyFolder}/hmy', 'blockchain', 'latest-headers', '--nod
 result_remote_shard_0 = run(remote_shard_0, stdout=PIPE, stderr=PIPE, universal_newlines=True)
 remote_data_shard_0 = json.loads(result_remote_shard_0.stdout)
 remote_shard = [f'{harmonyFolder}/hmy', 'blockchain', 'latest-headers', f'--node=https://api.s{ourShard}.t.hmny.io']
-result_remote_shard = run(remote_shard, stdout=PIPE, stderr=PIPE, universal_newlines=True)
-remote_data_shard = json.loads(result_remote_shard.stdout)
-local_shard = [f'{harmonyFolder}/hmy', 'blockchain', 'latest-headers']
-result_local_shard = run(local_shard, stdout=PIPE, stderr=PIPE, universal_newlines=True)
-local_data_shard = json.loads(result_local_shard.stdout)
-
-# get database sizes
-def getDBSize(ourShard, harmonyFolder) -> str:
-    checkSymlink = f'du -h {harmonyFolder}/harmony_db_{ourShard}'
-    if os.path.islink(checkSymlink):
-        harmonyFolder = os.path.relpath(checkSymlink)
-    harmonyDBSize = subprocess.getoutput(f"du -h {harmonyFolder}/harmony_db_{ourShard}")
-    harmonyDBSize = harmonyDBSize.rstrip('\t')
-    return harmonyDBSize[:-countTrim]
-
-# get shard stats
-def shardStats(ourShard) -> str:
-    ourUptime = subprocess.getoutput("uptime")
-    ourVersion = subprocess.getoutput(f"{harmonyFolder}/harmony -V")
-    dbZeroSize = getDBSize('0', harmonyFolder)
-    if ourShard == 0:
-        print(f"""
-* Uptime :: {ourUptime}
-* Harmony DB 0 Size  ::  {dbZeroSize}
+    try:
+        result_remote_shard = run(remote_shard, stdout=PIPE, stderr=PIPE, universal_newlines=True)
+        remote_data_shard = json.loads(result_remote_shard.stdout)
+    except (ValueError, KeyError, TypeError):
+        print(f'Remote API not responding')
+        return
+    local_shard = [f'{validatorToolbox.hmyAppPath}', 'blockchain', 'latest-headers']
+    try:
+        result_local_shard = run(local_shard, stdout=PIPE, stderr=PIPE, universal_newlines=True)
+        local_data_shard = json.loads(result_local_shard.stdout)
+    except (ValueError, KeyError, TypeError):
+        print(f'Local client not running')
+        return
+    print(f"""
+{stringStars()}
+* Current Date & Time: {timeNow}
 *
-* {ourVersion}
-***
-        """)
-    else:
-        print(f"""
-* Uptime :: {ourUptime}
+{stringStars()}
+* Current Status of our server {validatorToolbox.serverHostName} currently on Shard {environ.get('SHARD')}:
 *
-* Harmony DB 0 Size  ::  {dbZeroSize}
-* Harmony DB {ourShard} Size  ::   {getDBSize(str(ourShard), harmonyFolder)}
-*
-* {ourVersion}
-*
-***
-        """)
-
-print(f"""
-***
 * Shard 0 Sync Status:
 * Local Server  - Epoch {local_data_shard['result']['beacon-chain-header']['epoch']} - Shard {local_data_shard['result']['beacon-chain-header']['shardID']} - Block {literal_eval(local_data_shard['result']['beacon-chain-header']['number'])}
 * Remote Server - Epoch {remote_data_shard_0['result']['shard-chain-header']['epoch']} - Shard {remote_data_shard_0['result']['shard-chain-header']['shardID']} - Block {literal_eval(remote_data_shard_0['result']['shard-chain-header']['number'])}
-***
-""")
-if ourShard > 0:
-    print(f"""
-***
+*
+{stringStars()}
+    """)
+    if int(ourShard) > 0:
+        print(f"""
 * Shard {ourShard} Sync Status:
+*
 * Local Server  - Epoch {local_data_shard['result']['shard-chain-header']['epoch']} - Shard {local_data_shard['result']['shard-chain-header']['shardID']} - Block {literal_eval(local_data_shard['result']['shard-chain-header']['number'])}
 * Remote Server - Epoch {remote_data_shard['result']['shard-chain-header']['epoch']} - Shard {remote_data_shard['result']['shard-chain-header']['shardID']} - Block {literal_eval(remote_data_shard['result']['shard-chain-header']['number'])}
-***
+*
+{stringStars()}
     """)
+    shardStats(ourShard)
 
-# run it all
-shardStats(ourShard)
+runStats()
