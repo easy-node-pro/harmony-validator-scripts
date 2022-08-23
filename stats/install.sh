0aabbc9b8fd92c7bd48b7624a1e8c1f483ce4b6e
PatrickMogul <patrick@easynode.one>
2022-08-23 13:46:58 -0500

    Script

--- stats/install.sh
+++ stats/install.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-wget https://raw.githubusercontent.com/easy-node-one/harmony-validator-scripts/main/stats/stats.py -O stats.py && python3 stats.py
+sudo apt-get install dnsutils git python3-pip python3-dotenv -y && pip3 install colorama simple_term_menu && wget https://raw.githubusercontent.com/easy-node-one/harmony-validator-scripts/main/stats/stats.py -O stats.py && python3 stats.py
